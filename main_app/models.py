762b2e40747260ee79c379eb05aa90e6e55fa21f
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-04-14 19:32:39 -0600

    created model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Pedal(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    style = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    amperage = models.IntegerField()
+    voltage = models.IntegerField()
