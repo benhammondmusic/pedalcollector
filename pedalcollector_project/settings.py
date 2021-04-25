6e4204691d2c50b6a242fab635b51d845cf4e480
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-04-24 22:47:28 -0600

    fix settings

--- pedalcollector_project/settings.py
+++ pedalcollector_project/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
 from pathlib import Path
 from decouple import config
 import django_on_heroku
@@ -127,8 +126,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/pedals/'
 LOGOUT_REDIRECT_URL = '/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
